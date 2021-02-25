2b50fa779a7677fa758e780e3a2b792304d80998
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-02-24 21:49:09 -0300

    test: ensure SignUpController return 400 an error if no name is provied

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,5 +12,19 @@ describe('SignUp Controller', () => {
         }
         const htppResponse = sut.handle(httpRequest)
         expect(htppResponse.statusCode).toBe(400)
+        expect(htppResponse.body).toEqual(new Error('Missing param: name'))
+    })
+
+    test('Should return 400 if no name is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const htppResponse = sut.handle(httpRequest)
+        expect(htppResponse.statusCode).toBe(400)
     })
 })

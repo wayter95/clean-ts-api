e1a61c2ffdf0c99f5eea337ac1f2d349a89f8362
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-04 23:31:58 -0300

    feat: ensure LogMongoRepository creates an error log on success

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
-MongoHelper.conecct(env.mongoUrl)
+MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import ('./config/app')).default
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))

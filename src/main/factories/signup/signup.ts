716443174eb54f68da214f6ced9f7c17264536d9
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-07 18:42:07 -0300

    feat: add loginValidationFactorie

---
+++ src/main/factories/signup/signup.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { BcrypAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
+
+export const makeSignUpController = (): Controller => {
+  const salt = 12
+  const bcrypAdapter = new BcrypAdapter(salt)
+  const cccountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcrypAdapter,cccountMongoRepository)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController,logMongoRepository)
+}

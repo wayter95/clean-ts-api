ba3710d6a34cf0a0720f8cfed921340192d528b8
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-07 13:03:20 -0300

    chore: add moesif-nodejs

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,19 @@
 import { Express } from 'express'
 import { bodyParser, cors, contentType } from '../middlewares'
+const moesif = require('moesif-nodejs');
+
+const moesifMiddleware = moesif({
+  applicationId: 'eyJhcHAiOiIxOTg6MTE1NCIsInZlciI6IjIuMCIsIm9yZyI6Ijg4OjE2NjgiLCJpYXQiOjE2MTQ1NTY4MDB9.uGW0eB8XrvtDT3JMP4lItmDlBJ8pzOCS5u4-plA2ZPU',
+
+  // Optional hook to link API calls to users
+  identifyUser: function (req, res) {
+    return req.user ? req.user.id : undefined
+  }
+})
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
   app.use(contentType)
+  app.use(moesifMiddleware);
 }

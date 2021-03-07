62ea014c9bd22be2e9806d99d418ca953b482388
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-07 17:45:45 -0300

    test: ensure RequiredFieldValdiation not return if validaiton success

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should not return if validation success', () => {
+    const sut = new RequiredFielValidation('field')
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
 })

8b5178d81aad3924d032ab45bf43ce45f8c344fd
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-19 20:29:19 -0300

    refactor: separete factories in folder

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -1,6 +1,6 @@
-import { EmailValidation,RequiredFielValidation,ValidationComposite } from '../../../presentation/helpers/validators'
-import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
+import { EmailValidation,RequiredFielValidation,ValidationComposite } from '../../../../presentation/helpers/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []

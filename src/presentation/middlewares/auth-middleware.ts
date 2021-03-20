60e29dc047779ab54cbc5850af96cbf2dfc8d692
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-20 19:19:28 -0300

    test: ensure DbLoadAccountByToken throws if any dependency throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -14,9 +14,7 @@ export class AuthMiddleware implements Middleware {
       if (accessToken) {
         const account = await this.loadAccountByToken.load(accessToken, this.role)
         if (account) {
-          return ok({
-            accountId: account.id
-          })
+          return ok({ accountId: account.id })
         }
       }
       return forbidden(new AccessDeniedError())

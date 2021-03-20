f77c4ded224f1d72ba04e744cfd8a03c0e173d0a
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-20 20:00:13 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -108,5 +108,11 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email')
       expect(account.password).toBe('any_password')
     })
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByEmail('any_token')
+      expect(account).toBeFalsy()
+    })
   })
 })

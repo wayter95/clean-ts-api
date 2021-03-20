08c98d7f8786e9a3f373a2a6187a32c856373461
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-19 21:59:56 -0300

    refactor: move login and signup controller to a folder

--- src/main/factories/controllers/login/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login/login-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { EmailValidation,RequiredFielValidation,ValidationComposite } from '../../../../validation/validators'
+import { EmailValidation,RequiredFielValidation,ValidationComposite } from '../../../../../validation/validators'
 import { makeLoginValidation } from './login-validation-factory'
-import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidator } from '../../../../validation/protocols/email-validator'
+import { Validation } from '../../../../../presentation/protocols/validation'
+import { EmailValidator } from '../../../../../validation/protocols/email-validator'
 
-jest.mock('../../../../validation/validators/validation-composite')
+jest.mock('../../../../../validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
--- src/main/factories/controllers/login/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login/login-validation-factory.ts
@@ -1,6 +1,6 @@
-import { EmailValidation,RequiredFielValidation,ValidationComposite } from '../../../../validation/validators'
-import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
+import { EmailValidation,RequiredFielValidation,ValidationComposite } from '../../../../../validation/validators'
+import { Validation } from '../../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../../../infra/validators/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []

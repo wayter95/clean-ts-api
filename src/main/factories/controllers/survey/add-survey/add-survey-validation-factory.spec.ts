ff12f3344d8dfcb288129916413fc488406ff80f
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-20 11:17:56 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { RequiredFielValidation, ValidationComposite } from '../../../../../validation/validators'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+import { Validation } from '../../../../../presentation/protocols/validation'
+
+jest.mock('../../../../../validation/validators/validation-composite')
+
+describe('AddSurveyValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFielValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})

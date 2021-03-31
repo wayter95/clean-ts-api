e5543b1da80f5cf310484b600557329cd779e5e7
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-30 23:29:18 -0300

    docs: update login api docs

---
+++ src/main/docs/components/bad-request.ts
@@ -0,0 +1,11 @@
+export const badRequest = {
+  description: 'Requisição Inválida',
+  content: {
+    'application/json': {
+      schema: {
+        $ref: '#/schemas/error'
+      }
+    }
+  }
+
+}
---
+++ src/main/docs/components/server-error.ts
@@ -0,0 +1,11 @@
+export const serverError = {
+  description: 'Erro no servidor',
+  content: {
+    'application/json': {
+      schema: {
+        $ref: '#/schemas/error'
+      }
+    }
+  }
+
+}

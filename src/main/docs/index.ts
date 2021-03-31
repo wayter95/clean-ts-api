704a2ad066a6256428c294dd88fdc4ee48bf55be
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-03-30 22:32:24 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API referente ao curso do manguinho para realziar enquestes entre progamadores',
+    version: '1.0.0'
+  }
+}

aaa98eb6567872e29c0f48d5f25e98faafa4153b
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-04-04 22:42:49 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -5,6 +5,54 @@ export const surveyResultPath = {
     }],
     tags: ['Enquete'],
     summary: 'API para criar as resposta de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/saveSurveyParams'
+          }
+        }
+      }
+    },
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
     requestBody: {
       content: {
         'application/json': {

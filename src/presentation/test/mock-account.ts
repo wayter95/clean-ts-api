557c60547641e13457bb5d69e2f3c863fe47054a
Wayter Paulo <wayter.paulo.95@gmail.com>
2021-04-05 12:49:26 -0300

    feat: add name to authentication response

--- src/presentation/test/mock-account.ts
+++ src/presentation/test/mock-account.ts
@@ -1,5 +1,6 @@
 import { AddAccount, AddAccountParams } from '@/domain/usecases/account/add-account'
 import { Authentication, AuthenticationParams } from '@/domain/usecases/account/authentication'
+import { AuthenticationModel } from '@/domain/models/authentication'
 import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
 import { AccountModel } from '@/domain/models/account'
 import { mockAccountModel } from '@/domain/test'
@@ -17,11 +18,14 @@ export class AddAccountSpy implements AddAccount {
 
 export class AuthenticationSpy implements Authentication {
   authenticationParams: AuthenticationParams
-  token = faker.random.uuid()
+  authenticationModel = {
+    accessToken: faker.random.uuid(),
+    name: faker.name.findName()
+  }
 
-  async auth (authenticationParams: AuthenticationParams): Promise<string> {
+  async auth (authenticationParams: AuthenticationParams): Promise<AuthenticationModel> {
     this.authenticationParams = authenticationParams
-    return Promise.resolve(this.token)
+    return this.authenticationModel
   }
 }
 
